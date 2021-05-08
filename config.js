da21e7ef8635b3a52a1a952eaa2f649523014354
Rossano D'Angelo <rossanodan@gmail.com>
2021-05-08 17:09:15 +0100

    master: initial commit

---
+++ config.js
@@ -0,0 +1,51 @@
+'use strict';
+
+module.exports = {
+  url: 'https://lumen.netlify.com',
+  pathPrefix: '/',
+  title: 'Rossano D\'Angelo',
+  subtitle: 'A blog by Rossano D\'Angelo',
+  copyright: '© All rights reserved.',
+  disqusShortname: '',
+  postsPerPage: 4,
+  googleAnalyticsId: '',
+  useKatex: false,
+  menu: [
+    {
+      label: 'Articles',
+      path: '/',
+    },
+    {
+      label: 'About me',
+      path: '/pages/about',
+    },
+    {
+      label: 'Contact me',
+      path: '/pages/contacts',
+    },
+  ],
+  author: {
+    name: 'Rossano D\'Angelo',
+    photo: '/photo.jpg',
+    bio:
+      'Application engineer at The LEGO Group in London. I write React and GraphQL code and I like reading manga.',
+    contacts: {
+      email: 'rossanodan@gmail.com',
+      facebook: 'rossanodan',
+      telegram: '',
+      twitter: 'rossanodan',
+      github: 'rossanodan',
+      rss: '',
+      vkontakte: '',
+      linkedin: 'rossanodan',
+      instagram: 'rossanodan',
+      line: '',
+      gitlab: '',
+      weibo: '',
+      codepen: '',
+      youtube: '',
+      soundcloud: '',
+      medium: '',
+    },
+  },
+};
