9f3b88e6e951e5753d0485b60c2d9dd3e94a3443
Rossano D'Angelo <rossanodan@gmail.com>
2021-05-08 18:44:39 +0100

    master: changing site url

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-  url: 'https://lumen.netlify.com',
+  url: 'https://keen-keller-1aeadb.netlify.app/',
   pathPrefix: '/',
   title: 'Rossano D\'Angelo',
   subtitle: 'A blog by Rossano D\'Angelo',
