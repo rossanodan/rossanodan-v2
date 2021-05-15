211be38d1d5952f1202af7cfdff9f2db35c4aa25
Rossano D'Angelo <rossanodan@Rossanos-MacBook-Air.local>
2021-05-15 21:23:42 +0100

    Revert "master: working on dark theme"
    This reverts commit aaf079db7f4ce6bf14edce78bce647e035c16bb9.

--- config.js
+++ config.js
@@ -19,6 +19,10 @@ module.exports = {
       label: 'About me',
       path: '/pages/about',
     },
+    {
+      label: 'Contact me',
+      path: '/pages/contacts',
+    },
   ],
   author: {
     name: 'Rossano D\'Angelo',
